333113b7c92671c5312680334a39d7072f3b8f7b
yanhai <yanhaigz@163.com>
2019-05-04 11:02:47 +0800

    C++类的基础

---
+++ 02-class/dm02_classSeparateCompilation/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by YANHAI on 2019/5/4.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "teacher.h"
+
+int main()
+{
+    MyTeacher t1;
+    t1.setAge(20);
+    cout << "t1.getAge(): " << t1.getAge() << endl;
+    return 0;
+}
