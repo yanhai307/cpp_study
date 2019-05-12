e02496858d6890611b2460cdfa46be59de1e76ec
yanhai <yanhaigz@163.com>
2019-05-12 19:36:41 +0800

    C++构造函数

--- 02-class/dm04_objectOriented2/circle.cpp
+++ 02-class/dm04_objectOriented2/circle.cpp
@@ -5,7 +5,8 @@
 #include "circle.h"
 
 // 在圆内1，不在圆内0
-int Circle::judge(Point &p1) {
+int Circle::judge(Point &p1)
+{
     int r2 = m_r * m_r;
     int d2 = (p.getX() - p1.getX()) * (p.getX() - p1.getX()) +
              (p.getY() - p1.getY()) * (p.getY() - p1.getY());
